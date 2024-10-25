06649d9af154af3a81978279f1a19c049a244c95
gabrelso <146791410+gabrelso@users.noreply.github.com>
2024-10-25 00:21:37 -0300

    Update SystemBroken- (1).txt.lua

--- SystemBroken- (1).txt.lua
+++ SystemBroken- (1).txt.lua
@@ -1,4 +1,4 @@
-local player = game.Players.LocalPlayer
+local player = game.Players.LocalPlayer 
 local vim = game:GetService("VirtualInputManager")
 local TeleportService = game:GetService("TeleportService")
 local _wait = task.wait
@@ -20,7 +20,7 @@ function AutoKi()
                 vim:SendKeyEvent(true, "Q", false, game)
                 vim:SendKeyEvent(false, "Q", false, game)
             end
-            wait(0.1)
+            _wait(0.1)
         end
     end)
 end
@@ -59,7 +59,7 @@ Tab1:AddToggle({
         getgenv().autoE = ValueE
         if ValueE then
             while getgenv().autoE do
-                wait(0.1)
+                _wait(0.1)
                 vim:SendKeyEvent(true, "E", false, game)
                 vim:SendKeyEvent(false, "E", false, game)
             end
@@ -76,7 +76,7 @@ Tab1:AddToggle({
         getgenv().autoV = ValueV
         if ValueV then
             while getgenv().autoV do
-                wait(0.1)
+                _wait(0.1)
                 vim:SendKeyEvent(true, "V", false, game)
                 vim:SendKeyEvent(false, "V", false, game)
             end
@@ -119,7 +119,7 @@ Tab3:AddToggle({
     Callback = function(ValueDK)
         getgenv().destroyBlast = ValueDK
         while getgenv().destroyBlast do
-            wait(0.1)
+            _wait(0.1)
             for _, part in pairs(workspace.PartStorage:GetChildren()) do
                 part:Destroy()
             end
@@ -135,60 +135,70 @@ Tab2:AddButton({
         player.Character.HumanoidRootPart.CFrame = CFrame.new(2330.640869140625, 550.7536010742188, -1724.03173828125) 
     end 
 })
+
 Tab2:AddButton({ 
     Name = "TP Pumpkin Location 2", 
     Callback = function() 
         player.Character.HumanoidRootPart.CFrame = CFrame.new(1402.6492919921875, 552.6553344726562, 1864.331298828125) 
     end 
 })
+
 Tab2:AddButton({ 
     Name = "TP Pumpkin Location 3", 
     Callback = function() 
         player.Character.HumanoidRootPart.CFrame = CFrame.new(-287.9718322753906, 551.8124389648438, -369.174072265625) 
     end 
 })
+
 Tab2:AddButton({ 
     Name = "TP Pumpkin Location 4", 
     Callback = function() 
         player.Character.HumanoidRootPart.CFrame = CFrame.new(206.6642303466797, 550.7536010742188, -1967.5059814453125) 
     end 
 })
+
 Tab2:AddButton({ 
     Name = "TP Pumpkin Location 5", 
     Callback = function() 
         player.Character.HumanoidRootPart.CFrame = CFrame.new(776.6201782226562, 550.7536010742188, -1586.50634765625) 
     end 
 })
+
 Tab2:AddButton({ 
     Name = "TP Pumpkin Location 6", 
     Callback = function() 
         player.Character.HumanoidRootPart.CFrame = CFrame.new(2678.873291015625, 550.7536010742188, -994.9588623046875) 
     end 
 })
+
 Tab2:AddButton({ 
     Name = "TP Pumpkin Location 7", 
     Callback = function() 
         player.Character.HumanoidRootPart.CFrame = CFrame.new(2588.227294921875, 564.5017700195312, -4670.9208984375) 
     end 
 })
+
 Tab2:AddButton({ 
     Name = "TP Pumpkin Location 8", 
     Callback = function() 
         player.Character.HumanoidRootPart.CFrame = CFrame.new(-242.6627197265625, 3360.113525390625, -2763.47265625) 
     end 
 })
+
 Tab2:AddButton({ 
     Name = "TP Pumpkin Location 9", 
     Callback = function() 
         player.Character.HumanoidRootPart.CFrame = CFrame.new(-522.2780151367188, 645.3949584960938, -2774.021484375) 
     end 
 })
+
 Tab2:AddButton({ 
     Name = "TP Pumpkin Location 10", 
     Callback = function() 
         player.Character.HumanoidRootPart.CFrame = CFrame.new(417.4867248535156, 549.7522583007812, -3276.27490234375) 
     end 
 })
+
 Tab2:AddButton({
     Name = "TP All Pumpkin Locations",
     Callback = function()
@@ -206,7 +216,7 @@ Tab2:AddButton({
         }
         for _, loc in pairs(locations) do
             player.Character.HumanoidRootPart.CFrame = loc
-            wait(2)
+            _wait(2)
         end
     end
 })
