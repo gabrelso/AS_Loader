eed47a7b3a833dda397baa266e26cb14e0ea0357
gabrelso <146791410+gabrelso@users.noreply.github.com>
2024-10-29 03:34:52 -0300

    Update SystemBroken- (1).txt.lua

--- SystemBroken- (1).txt.lua
+++ SystemBroken- (1).txt.lua
@@ -8,7 +8,6 @@ local Window = Fluent:CreateWindow({
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
-    Theme = "Dark",
     MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
 })
 
